4f71b7f7e8d545ffdd455b051ef2b6e6f026d70d
Yegor Popinako <yegor5x@gmail.com>
2024-04-23 21:32:55 +0300

    initial commit

---
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.fitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button1 = findViewById(R.id.startfitness1);
+        button2 = findViewById(R.id.startfitness2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void beforeage18(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void afterage18(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity2.class);
+        startActivity(intent);
+    }
+
+    public void food(View view) {
+        Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/fitness/Splashscreen.java
@@ -0,0 +1,53 @@
+package com.example.fitness;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splashscreen extends AppCompatActivity {
+
+    Animation up, down;
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        ImageView imageView = findViewById(R.id.appsplash);
+        up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.up);
+        imageView.setAnimation(up);
+
+        TextView textView = findViewById(R.id.appname);
+        down = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.down);
+        textView.setAnimation(down);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3500);
+
+    }
+}

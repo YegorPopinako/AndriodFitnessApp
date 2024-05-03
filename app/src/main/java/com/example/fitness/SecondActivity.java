40e4a5459324a84d3351a65072aea248db060aa5
Yegor Popinako <yegor5x@gmail.com>
2024-05-03 22:48:23 +0300

    added third activity

--- app/src/main/java/com/example/fitness/SecondActivity.java
+++ app/src/main/java/com/example/fitness/SecondActivity.java
@@ -1,15 +1,20 @@
 package com.example.fitness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+
+    int[] newArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,38 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        newArray = new int[]{
+                R.id.bow_pose,
+                R.id.bridge_pose,
+                R.id.chair_pose,
+                R.id.child_pose,
+                R.id.cobbler_pose,
+                R.id.cow_pose,
+                R.id.playji_pose,
+                R.id.pauseji_pose,
+                R.id.plank_pose,
+                R.id.crunches_pose,
+                R.id.situp_pose,
+                R.id.rotation_pose,
+                R.id.twist_pose,
+                R.id.windmill_pose,
+                R.id.legup_pose
+        };
     }
 
     public void imageButtonClicked(View view) {
+        for(int i=0; i < newArray.length; i++){
+            if(view.getId() == newArray[i]){
+                int value = i+1;
+                Log.i("FIRST", String.valueOf(value));
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+
+            }
+        }
+
     }
 }

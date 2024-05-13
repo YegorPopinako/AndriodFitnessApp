5004a48c84f07f8b556695df2c3f751953e7943f
Yegor Popinako <yegor5x@gmail.com>
2024-05-13 15:43:10 +0300

    added Foods & Diet page

--- app/src/main/java/com/example/fitness/FoodActivity.java
+++ app/src/main/java/com/example/fitness/FoodActivity.java
@@ -1,6 +1,11 @@
 package com.example.fitness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FoodActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,38 @@ public class FoodActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String[] tstory = getResources().getStringArray(R.array.title_story);
+        final String[] dstory = getResources().getStringArray(R.array.details_story);
+
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row, R.id.rowtxt, tstory);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String t = dstory[position];
+                Intent intent = new Intent(FoodActivity.this, FoodActivityDetails.class);
+                intent.putExtra("story", t);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void foodGoBack(View view) {
+        Intent intent = new Intent(FoodActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(FoodActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/example/fitness/FoodActivityDetails.java
@@ -0,0 +1,48 @@
+package com.example.fitness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FoodActivityDetails extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_food_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.txt);
+        String dstory = getIntent().getStringExtra("story");
+        textView.setText(dstory);
+    }
+
+    public void goBack(View view) {
+
+        Intent intent = new Intent(FoodActivityDetails.this, FoodActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(FoodActivityDetails.this, FoodActivity.class);
+        startActivity(intent);
+        finish();
+        super.onBackPressed();
+    }
+}
